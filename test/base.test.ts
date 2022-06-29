a0b0c8527c1e160d74093bcd4d8f7c8120f07eaa
Dinesh A <dfa2222@gmail.com>
2022-06-28 21:06:21 -0400

    added desc test

--- test/base.test.ts
+++ test/base.test.ts
@@ -1,6 +1,11 @@
 import BaseSchema from "../src/base";
 
 describe("BaseSchema", () => {
+	it("BaseSchema.prototype.description", () => {
+		const schema = new BaseSchema().description("test");
+		expect(schema.plain.description).toEqual("test");
+	});
+
 	it("BaseSchema.prototype.optional", () => {
 		const schema = new BaseSchema();
 		expect(schema.isRequired).toEqual(true);
